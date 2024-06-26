e7f7ea6adcf897333384abeeb9229937cdda41b7
stalky <stalky7771@protonmail.com>
2024-06-26 22:20:18 +0300

    First commit, permutations func was added to math

---
+++ Math/MathResearch.cs
@@ -0,0 +1,33 @@
+﻿namespace Main.Math
+{
+	public class MathResearch
+	{
+		public static void PrintPermutations(char[] a, int i, int n)
+		{
+			int j;
+
+			if (i == n)
+			{
+				Console.WriteLine(new string(a));
+			}
+			else
+			{
+				char temp;
+				for (j = i; j <= n; j++)
+				{
+					// swap(a[i], a[j]);
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+
+					PrintPermutations(a, i + 1, n);
+
+					// swap(a[i], a[j]);
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Main.Math;
+
+namespace Main
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] a = "ABCD".ToCharArray();
+			MathResearch.PrintPermutations(a, 0, a.Length - 1);
+
+			Console.WriteLine("Finished");
+			Console.ReadKey();
+		}
+	}
+}
