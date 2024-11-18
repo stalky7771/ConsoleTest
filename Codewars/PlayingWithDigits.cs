61b7a84366a5acc919a23ecdd2154f54486b1648
stalky <stalky7771@protonmail.com>
2024-11-18 23:57:47 +0200

    AesEncryptamajig was added

---
+++ Codewars/PlayingWithDigits.cs
@@ -0,0 +1,28 @@
+﻿// https://www.codewars.com/kata/5552101f47fc5178b1000050/csharp
+using static System.Math;
+
+namespace Main.Codewars
+{
+	public class PlayingWithDigits
+	{
+		public static long digPow_V1(int n, int p)
+		{
+			var sum = Convert.ToInt64(n.ToString().Select(s => Pow(int.Parse(s.ToString()), p++)).Sum());
+			return sum % n == 0 ? sum / n : -1;
+		}
+
+		public static long digPow_V2(int n, int p)
+		{
+			var value = n.ToString()
+				.ToCharArray()
+				.Select(c => int.Parse(c.ToString()))
+				.Select((d, idx) => Pow(d, idx + p))
+				.Sum();
+
+			if ((value % n) != 0)
+				return -1;
+
+			return (long)value / n;
+		}
+	}
+}
---
+++ Codewars/TribonacciSequence.cs
@@ -0,0 +1,19 @@
+﻿// https://www.codewars.com/kata/556deca17c58da83c00002db/csharp
+
+namespace Main.Codewars
+{
+	public class TribonacciSequence
+	{
+		public double[] Tribonacci(double[] signature, int n)
+		{
+			var seq = new List<double>(signature);
+
+			for (var i = 3; i < n; i++)
+			{
+				seq.Add(seq[i - 1] + seq[i - 2] + seq[i - 3]);
+			}
+
+			return seq.Take(n).ToArray();
+		}
+	}
+}
