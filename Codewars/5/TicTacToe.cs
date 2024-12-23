77ef8da1a9be0818915b5fe6b0c8378144721567
stalky <stalky7771@protonmail.com>
2024-12-23 17:59:00 +0200

    TicTacToe was changed

--- Codewars/5/TicTacToe.cs
+++ Codewars/5/TicTacToe.cs
@@ -7,13 +7,13 @@ namespace Main.Codewars
 		public int IsSolved(int[,] board)
 		{
 			var b = board;
-			for (var i = 0; i < 2; i++)
+			for (var i = 0; i < 3; i++)
 			{
 				if (b[i, 0] != 0 && b[i, 0] == b[i, 1] && b[i, 0] == b[i, 2])
 					return b[i, 0];
 			}
 
-			for (var i = 0; i < 2; i++)
+			for (var i = 0; i < 3; i++)
 			{
 				if (b[0, i] != 0 && b[0, i] == b[1, i] && b[0, i] == b[2, i])
 					return b[0, i];
@@ -25,7 +25,12 @@ namespace Main.Codewars
 			if (b[0, 2] != 0 && b[0, 2] == b[1, 1] && b[1, 1] == b[2, 0])
 				return b[0, 2];
 
-
+			for (var i = 0; i < 3; i++)
+			{
+				for (var j = 0; j < 3; j++)
+					if (b[i, j] == 0)
+						return -1;
+			}
 
 			return 0;
 		}
